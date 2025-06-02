02aa41b326e634f9fce7659d90f278bf094d71e6
Benedict <chensxb97@gmail.com>
2025-06-02 22:29:28 +0800

    added blackbox and prometheus configurations

--- backend/main.go
+++ backend/main.go
@@ -14,8 +14,36 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 func targetsHandler(w http.ResponseWriter, r *http.Request) {
 	targets := []map[string]interface{}{
 		{"targets": []string{"http://example.com"}},
-		{"targets": []string{"http://example.com2"}},
-		{"targets": []string{"https://google.com"}},
+		{"targets": []string{"http://localhost:8000"}},
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(targets); err != nil {
+		http.Error(w, "Failed to encode targets", http.StatusInternalServerError)
+	}
+}
+
+func targetsHandlerWithCustomLabels(w http.ResponseWriter, r *http.Request) {
+	targets := []map[string]interface{}{
+		{
+			"targets": []string{"http://example.com"},
+			"labels": map[string]string{
+				"__meta_module":      "http_2xx",
+				"__meta_application": "App 1",
+				"__meta_team":        "Alpha",
+				"__meta_environment": "prod",
+				"__meta_datacenter":  "A",
+			},
+		},
+		{
+			"targets": []string{"http://localhost:8000"},
+			"labels": map[string]string{
+				"__meta_module":      "http_2xx",
+				"__meta_application": "App 2",
+				"__meta_team":        "Beta",
+				"__meta_environment": "nonprod",
+				"__meta_datacenter":  "B",
+			},
+		},
 	}
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(targets); err != nil {
@@ -26,6 +54,7 @@ func targetsHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/targets", targetsHandler)
+	http.HandleFunc("/targets_custom", targetsHandlerWithCustomLabels)
 
 	protocol := "http"
 	host := "localhost"
